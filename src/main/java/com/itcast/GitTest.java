04ebaa45913ce184bfc345cd9d418387294cdaff
lr_glb <lr799437029@aliyun.com>
2019-04-02 15:25:00 +0800

    first person's modification

--- src/main/java/com/itcast/GitTest.java
+++ src/main/java/com/itcast/GitTest.java
@@ -3,5 +3,6 @@ package com.itcast;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是测试git...");
+        System.out.println("这是第一个人的修改");
     }
 }
