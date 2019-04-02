19b841fdf6a5b26a747d08cd068012fc67107ca7
lr_glb <lr799437029@aliyun.com>
2019-04-02 15:41:02 +0800

    first commit idea git test

--- src/main/java/com/itcast/GitTest.java
+++ src/main/java/com/itcast/GitTest.java
@@ -3,5 +3,6 @@ package com.itcast;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是测试git...");
+        System.out.println("第二个人的修改，哈哈哈");
     }
 }
